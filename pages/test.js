eb622131c306dc549040c427eef093d6e13a7bd8
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-04-28 00:51:04 +0530

    removed test.js

--- pages/test.js
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import Event from "../components/event";
-import  Footer  from "../components/footer";
-export default function Main() {
-    return <>
-    <Footer/>
-    </>;
-}
