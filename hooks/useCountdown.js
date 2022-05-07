69064057651d5bd0c405ef0422287ba01bbe1a6f
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-05-07 16:20:36 +0530

    const day error fixed

--- hooks/useCountdown.js
+++ hooks/useCountdown.js
@@ -20,12 +20,12 @@ const useCountdown = (targetDate) => {
 
 const getReturnValues = (countDown) => {
   // calculate time left
-  const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
-  const hours = Math.floor(
+  let days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+  let hours = Math.floor(
     (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
   );
-  const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
-  const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+  let minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((countDown % (1000 * 60)) / 1000);
   if (seconds < 10) { 
     seconds = "0"+seconds;
   }
