f539dee32f0fa3c132235eb1169cf752fc1d6db7
Abhinav Kumar <akum19cs@cmrit.ac.in>
2022-05-07 14:10:45 +0530

    added responsive map

--- pages/index.js
+++ pages/index.js
@@ -33,16 +33,13 @@ export default function Main() {
       <About />
       <div className="grid sm:grid-2 lg:grid-cols-2 gap-3 md:grid-cols-2">
         <Footer />
-        <div className="" style={{ marginTop: "2em" , marginLeft: "10em" }}>
+        <div className="p-1 m-2">
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.0682709249145!2d77.7096099348877!3d12.9674832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae130f321b2b95%3A0x74b2c05bbc2aac8d!2sCMR%20Institute%20of%20Technology!5e0!3m2!1sen!2sin!4v1651911092198!5m2!1sen!2sin"
-            width="80%"
-            height="90%"
-            frameborder="0"
-            style={{ border: 0 }}
+            width="100%"
+            height="380"
+            
             allowfullscreen=""
-            aria-hidden="false"
-            tabindex="0"
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
