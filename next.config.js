d48a8836e923fa86b1e2863f2f4372963844239c
Nalin Singh <nalin@192.168.1.2>
2022-05-09 09:55:08 +0530

    Dynamic Images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	images: { domains: ["github.com"] },
+	images: { domains: ["cmritcultura.com"] },
 };
 
 module.exports = nextConfig;
